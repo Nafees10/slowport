b20ff64aa7577e00e158171681798573d2741a0f
Nafees Hassan <nafeeshassan16@gmail.com>
2023-11-30 15:07:14 +0500

    Implemented missing functions

--- app/src/main/java/slowport/db/NoteDB.java
+++ app/src/main/java/slowport/db/NoteDB.java
@@ -5,22 +5,28 @@ import java.util.*;
 import slowport.common.*;
 
 public class NoteDB{
-	private static String queryGetNotes =
+	private static final String queryGetNotes =
 """
 SELECT week, course, sessionIndex, note FROM notes;
 """;
 
-	private static String queryGetNotesByCourse =
+	private static final String queryGetNotesByCourse =
 """
 SELECT week, course, sessionIndex, note FROM notes WHERE course=?;
 """;
 
-	private static String queryAddNote =
+	private static final String queryAddNote =
 """
 INSERT INTO notes (week, course, sessionIndex, note) VALUES (?, ?, ?, ?);
 """;
 
-	private PreparedStatement stmntGetNotes, stmntGetNotesByCourse, stmntAddNote;
+	private static final String queryRemoveNote =
+"""
+DELETE FROM notes WHERE week=? AND course=? AND sessionIndex=?;
+""";
+
+	private PreparedStatement stmntGetNotes, stmntGetNotesByCourse, stmntAddNote,
+					stmntRemoveNote;
 
 	private Note create(ResultSet res){
 		try{
@@ -34,6 +40,7 @@ INSERT INTO notes (week, course, sessionIndex, note) VALUES (?, ?, ?, ?);
 					);
 			return note;
 		} catch (SQLException e){
+			e.printStackTrace();
 			return null;
 		}
 	}
@@ -43,19 +50,70 @@ INSERT INTO notes (week, course, sessionIndex, note) VALUES (?, ?, ?, ?);
 			stmntGetNotes = conn.prepareStatement(queryGetNotes);
 			stmntGetNotesByCourse = conn.prepareStatement(queryGetNotesByCourse);
 			stmntAddNote = conn.prepareStatement(queryAddNote);
-		} catch (SQLException e ){
+			stmntRemoveNote = conn.prepareStatement(queryRemoveNote);
+		} catch (SQLException e){
 			e.printStackTrace();
 			throw new DBException();
 		}
 	}
 
 	public List<Note> getNotes(){
-		List<Note> ret = null;
 		try{
 			ResultSet res = stmntGetNotes.executeQuery();
-			Note note = create(res);
-			ret; /// TODO continue from here
+			List<Note> ret = new ArrayList<>();
+			while (true){
+				Note note = create(res);
+				if (note == null)
+					break;
+				ret.add(note);
+			}
+			return ret;
+		} catch (SQLException e){
+			e.printStackTrace();
 		}
-		return ret;
+		return null;
+	}
+
+	public List<Note> getNote(String course){
+		try{
+			stmntGetNotesByCourse.setString(1, course);
+			ResultSet res = stmntGetNotesByCourse.executeQuery();
+			List<Note> ret = new ArrayList<>();
+			while (true){
+				Note note = create(res);
+				if (note == null)
+					break;
+				ret.add(note);
+			}
+			return ret;
+		} catch (SQLException e){
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public boolean addNote(Note note){
+		try{
+			stmntAddNote.setInt(1, note.getWeek());
+			stmntAddNote.setString(2, note.getCourse());
+			stmntAddNote.setInt(3, note.getSessionIndex());
+			stmntAddNote.setString(4, note.getNote());
+			return stmntAddNote.executeUpdate() >= 1;
+		} catch (SQLException e){
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	public boolean removeNote(Note note){
+		try{
+			stmntRemoveNote.setInt(1, note.getWeek());
+			stmntRemoveNote.setString(2, note.getCourse());
+			stmntRemoveNote.setInt(3, note.getSessionIndex());
+			return stmntRemoveNote.executeUpdate() >= 1;
+		} catch (SQLException e){
+			e.printStackTrace();
+		}
+		return false;
 	}
 }
