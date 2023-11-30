7ae632fd0e228c3d612b10627f93df2fa310697f
Nafees Hassan <nafeeshassan16@gmail.com>
2023-11-30 15:06:52 +0500

    Implemented TimetableDB class

--- app/src/main/java/slowport/db/TimetableDB.java
+++ app/src/main/java/slowport/db/TimetableDB.java
@@ -2,3 +2,74 @@ package slowport.db;
 
 import java.sql.*;
 import slowport.common.*;
+
+import java.util.*;
+
+public class TimetableDB{
+	private static final String queryGetVersions =
+"""
+SELECT DISTINCT version FROM timetables;
+""";
+
+	private static final String queryGetTimetable =
+"""
+SELECT timetable FROM timetables WHERE version=?;
+""";
+
+	private static final String queryAddTimetable =
+"""
+INSERT INTO timetables (version, timetable) VALUES (?, ?);
+""";
+
+	private PreparedStatement stmntGetVersions, stmntGetTimetable,
+					stmntAddTimetable;
+	private HashMap<String, List<Session>> sessions;
+
+	public TimetableDB(Connection conn) throws DBException{
+		try{
+			stmntGetVersions = conn.prepareStatement(queryGetVersions);
+			stmntGetTimetable = conn.prepareStatement(queryGetTimetable);
+			stmntAddTimetable = conn.prepareStatement(queryAddTimetable);
+		} catch (SQLException e){
+			e.printStackTrace();
+			throw new DBException();
+		}
+	}
+
+	public List<String> getVersions(){
+		try{
+			ResultSet res = stmntGetVersions.executeQuery();
+			List<String> ret = new ArrayList<>();
+			while (res.next())
+				ret.add(res.getString(1));
+			return ret;
+		} catch (SQLException e){
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public String getTimetable(String version){
+		try{
+			stmntGetTimetable.setString(1, version);
+			ResultSet res = stmntGetTimetable.executeQuery();
+			if (!res.next())
+				return null;
+			return res.getString(1);
+		} catch (SQLException e){
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public boolean addTimetable(String version, String timetable){
+		try{
+			stmntAddTimetable.setString(1, version);
+			stmntAddTimetable.setString(2, timetable);
+			return stmntAddTimetable.executeUpdate() >= 1;
+		} catch (SQLException e){
+			e.printStackTrace();
+		}
+		return false;
+	}
+}
