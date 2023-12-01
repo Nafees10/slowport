89cdca56b3a386a78784010fb8978ae7d3ed69b4
Nafees Hassan <nafeeshassan16@gmail.com>
2023-12-01 16:45:51 +0500

    int week is now of type YearWeek

--- app/src/main/java/slowport/common/Note.java
+++ app/src/main/java/slowport/common/Note.java
@@ -1,13 +1,13 @@
 package slowport.common;
 
 public class Note{
-	private int week;
+	private YearWeek week;
 	private String course;
 	private String section;
 	private int sessionIndex;
 	private String note;
 
-	public Note(int week, String course, String section, int sessionIndex,
+	public Note(YearWeek week, String course, String section, int sessionIndex,
 			String note){
 		this.week = week;
 		this.course = course;
@@ -16,43 +16,23 @@ public class Note{
 		this.note = note;
 	}
 
-	public int getWeek() {
+	public YearWeek getWeek() {
 		return week;
-	}
-
-	public void setWeek(int week) {
-		this.week = week;
 	}
 
 	public String getCourse() {
 		return course;
 	}
 
-	public void setCourse(String course) {
-		this.course = course;
-	}
-
 	public String getSection() {
 		return section;
-	}
-
-	public void setSection(String section) {
-		this.section = section;
 	}
 
 	public int getSessionIndex() {
 		return sessionIndex;
 	}
 
-	public void setSessionIndex(int sessionIndex) {
-		this.sessionIndex = sessionIndex;
-	}
-
 	public String getNote() {
 		return note;
-	}
-
-	public void setNote(String note) {
-		this.note = note;
 	}
 }
