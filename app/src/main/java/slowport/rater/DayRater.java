0571455fccf65aadd77eeeb0d3584604ab4eed80
Nafees Hassan <nafeeshassan16@gmail.com>
2023-12-01 21:34:53 +0500

    Implemented DayRater

--- app/src/main/java/slowport/rater/DayRater.java
+++ app/src/main/java/slowport/rater/DayRater.java
@@ -1,10 +1,18 @@
 package slowport.rater;
 
+import java.time.*;
 import java.util.*;
 import slowport.common.*;
 
 public class DayRater extends Rater{
 	public int rate(List<Session> sessions){
-		return 0; /// TODO
+		Map<DayOfWeek, Boolean> dayStatus = new HashMap<>();
+		for (Session session : sessions)
+			dayStatus.put(session.getDay(), true);
+		int days = dayStatus.size();
+		if (days > 7) // HOW??
+			return 0;
+		days = 7 - days;
+		return days * (1000 / 7);
 	}
 }
