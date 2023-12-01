c45088eaa33c66a99b01457573b0bcf42a8dd702
Nafees Hassan <nafeeshassan16@gmail.com>
2023-12-01 09:40:10 +0500

    Added queries for SelectionDB

--- app/src/main/java/slowport/db/SelectionDB.java
+++ app/src/main/java/slowport/db/SelectionDB.java
@@ -1,4 +1,26 @@
 package slowport.db;
 
+import java.util.*;
 import java.sql.*;
 import slowport.common.*;
+
+public class SelectionDB{
+	private static final String queryGetSelections =
+"""
+SELECT course, section FROM selections;
+""";
+
+	private static final String queryRemoveSelection =
+"""
+DELETE FROM selections WHERE course=?;
+""";
+
+	private static final String queryAddSelection =
+"""
+INSERT INTO selections (course, section) VALUES (?, ?);
+""";
+
+	private PreparedStatement stmntGetSelections, stmntRemoveSelection,
+					stmntAddSelection;
+
+}
