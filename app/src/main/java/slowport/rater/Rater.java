a387992340d2e48d2c83bba8c97232c08d4e467a
Nafees Hassan <nafeeshassan16@gmail.com>
2023-12-02 00:05:46 +0500

    Implemented normalize

--- app/src/main/java/slowport/rater/Rater.java
+++ app/src/main/java/slowport/rater/Rater.java
@@ -4,8 +4,18 @@ import java.util.*;
 import slowport.common.*;
 
 public abstract class Rater{
+	private final static long RANGE_MAX = 1000, RANGE_MIN = 0;
 	public abstract long rate(Timetable timetable);
-	public static void normalize(List<Integer> scores){
-		// TODO
+	public static void normalize(List<Long> scores){
+		long min = scores.get(0), max = scores.get(0);
+		for (long score : scores){
+			min = min > score ? score : min;
+			max = max < score ? score : max;
+		}
+		// subtract toSub from all, multiply each with (RANGE_MAX / (max - toSub))
+		long toSub = min - RANGE_MIN,
+				 multiplier = RANGE_MAX / (max - toSub);
+		for (int i = 0; i < scores.size(); i ++)
+			scores.set(i, (scores.get(i) - toSub) * multiplier);
 	}
 }
