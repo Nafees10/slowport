f815f15d3fc9b98b9f6f5296f027735afd17791f
Nafees Hassan <nafeeshassan16@gmail.com>
2023-12-02 20:17:17 +0500

    Tabs > Spaces

--- app/src/main/java/slowport/filter/Combinator.java
+++ app/src/main/java/slowport/filter/Combinator.java
@@ -19,20 +19,20 @@ public class Combinator {
 	}
 
 	public List<List<Session>> combinations(List<Session> sessions) {
-        List<List<Session>> result = new ArrayList<>();
-        int totalSessions = sessions.size();
-        int totalCombinations = 1 << totalSessions; // 2^totalSessions
+		List<List<Session>> result = new ArrayList<>();
+		int totalSessions = sessions.size();
+		int totalCombinations = 1 << totalSessions; // 2^totalSessions
 
-        for (int i = 0; i < totalCombinations; i++) {
-            List<Session> currentCombination = new ArrayList<>();
-            for (int j = 0; j < totalSessions; j++) {
-                if ((i & (1 << j)) != 0) {
-                    currentCombination.add(sessions.get(j));
-                }
-            }
-            result.add(currentCombination);
-        }
+		for (int i = 0; i < totalCombinations; i++) {
+			List<Session> currentCombination = new ArrayList<>();
+			for (int j = 0; j < totalSessions; j++) {
+				if ((i & (1 << j)) != 0) {
+					currentCombination.add(sessions.get(j));
+				}
+			}
+			result.add(currentCombination);
+		}
 
-        return result;
-    }
+		return result;
+	}
 }
