c56c77522adff15071ec52df8a82be7f9e89a784
Muhammad Usman Khan <109995681+MuhammadUsmanAmjad@users.noreply.github.com>
2023-12-03 19:38:58 +0500

    Update SlowApi.java
    added the code for get versions using the http request and imported necessary flibraries for it

--- app/src/main/java/slowport/slowapi/SlowApi.java
+++ app/src/main/java/slowport/slowapi/SlowApi.java
@@ -1,5 +1,8 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.List;
-
 package slowapi;
 
 public class SlowApi {
@@ -17,7 +20,18 @@ public class SlowApi {
 
     // Method to get versions
     public static List<String> getVersions() {
-     
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(apiEndpoint + "/versions"))
+                .build();
+
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            return List.of(response.body().split("\n"));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return List.of();
+        }
     }
 
     // Method to get timetable for a specific version
