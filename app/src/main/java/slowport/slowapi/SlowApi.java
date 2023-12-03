30b5894a856206a9aa03335185351da6d2396e98
Muhammad Usman Khan <109995681+MuhammadUsmanAmjad@users.noreply.github.com>
2023-12-03 19:09:57 +0500

    Update SlowApi.java
    added the getter and setters for the dummy code for the rest of the functions

--- app/src/main/java/slowport/slowapi/SlowApi.java
+++ app/src/main/java/slowport/slowapi/SlowApi.java
@@ -1,5 +1,32 @@
-package slowport.slowapi;
+import java.util.List;
 
-public class SlowApi{
+package slowapi;
 
+public class SlowApi {
+    private static String apiEndpoint;
+
+    // Setter for apiEndpoint
+    public static void setApiEndpoint(String endpoint) {
+        apiEndpoint = endpoint;
+    }
+
+    // Getter for apiEndpoint
+    public static String getApiEndpoint() {
+        return apiEndpoint;
+    }
+
+    // Method to get versions
+    public static List<String> getVersions() {
+     
+    }
+
+    // Method to get timetable for a specific version
+    public static String getTimetable(String version) {
+       
+    }
+
+    // Method to get makeup timetable
+    public static String getMakeupTimetable() {
+        
+    }
 }
